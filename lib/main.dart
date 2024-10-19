f473c9c3153e06078831a2563c4e73fe7e33a9e2
makenana <makenabambrick17@gmail.com>
2024-10-18 19:00:10 -0700

    (10/18/24) added a navigation bar and created empty pages for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,83 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
+import 'dictionary.dart';
+import 'gardening_tips.dart';
+import 'my_plants.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Gardening App'
-        home: GardenApp(),
-  ));
+  runApp(GardenApp());
 }
 
-class GardenApp extends StatefulWidget {
-  const GardenApp({Key? key}) : super(key: key);
+class GardenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gardening App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.green[50],
+          selectedItemColor: Colors.green[800],
+          unselectedItemColor: Colors.green[400],
+        ),
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    DictionaryPage(),
+    GardeningTipsPage(),
+    MyPlantsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
-  GardenAppState createState() => GardenAppState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Gardening App'),
+              backgroundColor: Colors.green,
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Dictionary',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fence),
+            label: 'Gardening Tips',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.yard),
+            label: 'My Plants',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
 }
-
-class GardenAppState extends State<GardenApp> {
-
-
-}
-
---
+++ lib/my_plants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyPlantsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('This will be the my plants page'));
+  }
+}
